99905b28ca38ea71c568c5421ffc7eb2db5bd46d
Enrique Martinez <112442527+Trey3mtz@users.noreply.github.com>
2023-12-10 02:49:46 -0600

    Day and Night Cycling added
    Added changing time of day

--- Senior Project Game/Assets/Scripts/Systems Scripts/Day_Cycle.cs
+++ Senior Project Game/Assets/Scripts/Systems Scripts/Day_Cycle.cs
@@ -2,12 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace WorldTime
 {
     public class Day_Cycle : MonoBehaviour
     {
+        private Light2D _light;
 
+        [SerializeField]
+        private Time_World world_time;
+
+        [SerializeField]
+        private Gradient gradient;
+
+        private void Awake()
+        {
+            _light = GetComponent<Light2D>();
+            world_time.WorldTimeChanged += OnWorldTimeChanged;
+        }
+
+        private void OnDestroy()
+        {
+            world_time.WorldTimeChanged -= OnWorldTimeChanged;
+        }
+
+        private void OnWorldTimeChanged(object sender, TimeSpan newTime)
+        {
+
+            _light.color = gradient.Evaluate(PercentOfDay(newTime));
+        }
+
+        private float PercentOfDay(TimeSpan timeSpan)
+        {
+            return(float)timeSpan.TotalMinutes % Time_Constants.MinutesInDay / Time_Constants.MinutesInDay;
+        }
     }
 }
 
